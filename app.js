4570d6ae925191fb2c300f345813365dd65c1b86
Ahrorbek <123533812+HamidovAhrorbek@users.noreply.github.com>
2025-07-21 15:49:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const squares = documnet.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const tileLeft = documnet.querySelector('#time-left')
+const score = document.querySelector('#score')
 
+let result = 0
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomPosition = squares[0]
+}
